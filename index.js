92ca0b89c1929fc689ca0acc47d9f44252a3f1fb
Charles Lehner <cel@celehner.com>
2015-12-18 23:22:39 -0500

    Fix wrapping behavior

--- index.js
+++ index.js
@@ -53,40 +53,32 @@ function shouldDescendInto(node) {
 	return node.nodeName != "SCRIPT" && node.nodeName != "STYLE";
 }
 
-function getNextTextNode(node, container, wrap) {
-	outer: do {
+function getNextTextNode(node, container) {
+	do {
 		if (shouldDescendInto(node) && node.firstChild) {
 			node = node.firstChild;
-		} else if (node.nextSibling) {
-			node = node.nextSibling;
 		} else {
-			do {
-				if (node == container) {
-					if (!wrap)
-						return null;
-					wrap = false;
-					continue outer;
-				}
+			while (!node.nextSibling) {
 				node = node.parentNode;
-				if (!node)
-					node = container;
-			} while (!node.nextSibling);
+				if (node == container || !node)
+					return null;
+			}
 			node = node.nextSibling;
 		}
 	} while (node.nodeType != node.TEXT_NODE);
 	return node;
 }
 
-function getPreviousTextNode(node, container, wrap) {
+function getPreviousTextNode(node, container) {
+	if (node == container) {
+		while (node.lastChild && shouldDescendInto(node))
+			node = node.lastChild;
+		if (node.nodeType == node.TEXT_NODE)
+			return node;
+	}
 	do {
-		if (!node) {
-			node = container;
-		} else if (node == container) {
-			if (!wrap)
-				return null;
-			while (shouldDescendInto(node) && node.lastChild)
-				node = node.lastChild;
-			wrap = false;
+		if (!node || node == container) {
+			return null;
 		} else if (node.previousSibling) {
 			node = node.previousSibling;
 			while (shouldDescendInto(node) && node.lastChild)
@@ -114,14 +106,14 @@ TextNodeSearcher.prototype.selectNext = function () {
 	var startNode = sel.focusNode;
 	var startOffset = 0;
 	if (!startNode || !this.container.contains(startNode))
-		startNode = getNextTextNode(this.container, this.container, true);
+		startNode = getNextTextNode(this.container, this.container);
 	else if (startNode.nodeType != startNode.TEXT_NODE)
-		startNode = getNextTextNode(startNode, this.container, true);
+		startNode = getNextTextNode(startNode, this.container);
 	else
 		startOffset = sel.focusOffset;
 
-	for (var node = startNode; node;
-			node = getNextTextNode(node, this.container, true)) {
+	var wrapped = false;
+	for (var node = startNode; node;) {
 		var str = node.data;
 		this.query.lastIndex = startOffset;
 		if (startOffset)
@@ -131,6 +123,13 @@ TextNodeSearcher.prototype.selectNext = function () {
 			setSelection(node, m.index, node, m.index + m[0].length);
 			node.parentNode.scrollIntoView(false);
 			return;
+		}
+		node = getNextTextNode(node, this.container);
+		if (!node) {
+			if (wrapped)
+				return;
+			wrapped = true;
+			node = getNextTextNode(this.container, this.container);
 		}
 	}
 };
@@ -143,14 +142,14 @@ TextNodeSearcher.prototype.selectPrevious = function () {
 	var endNode = sel.anchorNode;
 	var endOffset = 0;
 	if (!endNode || !this.container.contains(endNode))
-		endNode = getPreviousTextNode(endNode, this.container, true);
+		endNode = getPreviousTextNode(this.container, this.container);
 	else if (endNode.nodeType != endNode.TEXT_NODE)
-		endNode = getPreviousTextNode(this.container, this.container, true);
+		endNode = getPreviousTextNode(endNode, this.container);
 	else
 		endOffset = sel.anchorOffset;
 
-	for (var node = endNode; node;
-			node = getPreviousTextNode(node, this.container, true)) {
+	var wrapped = false;
+	for (var node = endNode; node;) {
 		var str = node.data;
 		if (endOffset < Infinity) {
 			str = node.data.substr(0, endOffset);
@@ -159,8 +158,15 @@ TextNodeSearcher.prototype.selectPrevious = function () {
 		var m = matchLast(this.query, str);
 		if (m) {
 			setSelection(node, m.index, node, m.index + m[0].length);
-			node.parentNode.scrollIntoView(true);
+			node.parentNode.scrollIntoView(false);
 			return;
+		}
+		node = getPreviousTextNode(node, this.container);
+		if (!node) {
+			if (wrapped)
+				return;
+			wrapped = true;
+			node = getPreviousTextNode(this.container, this.container);
 		}
 	}
 };
